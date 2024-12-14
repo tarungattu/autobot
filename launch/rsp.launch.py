9f3a4c82c2305ea01202630cf08f1fbff854d7ea
tarungattu <tarunrgattu@gmail.com>
2024-12-14 18:58:17 +0530

    added new world files

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('autobot'))
-    xacro_file = os.path.join(pkg_path,'urdf','robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
 
     # FOR XACRO
     robot_description_config = xacro.process_file(xacro_file).toxml()
